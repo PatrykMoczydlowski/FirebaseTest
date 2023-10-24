d495f0b130357b0ea5ac4aa3302e19412f620e76
Patryk Mocczydlowski <pmoczydlowski5523@stu.d214.org>
2023-10-24 10:21:06 -0500

    Initial Commit

---
+++ Firebase/FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseApp.swift
+//  Firebase
+//
+//  Created by Patryk Moczydlowski on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
